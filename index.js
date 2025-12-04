4ed5870c5e8759ca08467daf60410c08737a8a68
DanielBekelee <daniel.bekele.garedew@gmail.com>
2025-12-04 11:26:27 +0300

    more version

--- index.js
+++ index.js
@@ -2,8 +2,21 @@ function sum(a, b) {
   return a + b;
 }
 
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  if (b === 0) throw new Error("Cannot divide by zero");
+  return a / b;
+}
+
 console.log("Hello from Node.js DevOps demo!");
 console.log("Sum example:", sum(5, 7));
-console.log("This is a sample application to demonstrate ESLint configuration.");
 
-module.exports = { sum };
+module.exports = { sum, subtract, multiply, divide };
+
--- tests/sum.test.js
+++ tests/sum.test.js
@@ -1,5 +1,21 @@
-const { sum } = require("../index");
+const { sum, subtract, multiply, divide } = require("../index");
 
 test("adds numbers", () => {
   expect(sum(5, 7)).toBe(12);
+});
+
+test("subtracts numbers", () => {
+  expect(subtract(10, 4)).toBe(6);
+});
+
+test("multiplies numbers", () => {
+  expect(multiply(3, 5)).toBe(15);
+});
+
+test("divides numbers", () => {
+  expect(divide(10, 2)).toBe(5);
+});
+
+test("division by zero throws", () => {
+  expect(() => divide(5, 0)).toThrow("Cannot divide by zero");
 });
