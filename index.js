9233e4fd3185fa2bfd68e8b0144418babbae07da
DanielBekelee <daniel.bekele.garedew@gmail.com>
2025-12-04 11:01:38 +0300

    Final project setup with working ESLint and tests

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ function sum(a, b) {
 
 console.log("Hello from Node.js DevOps demo!");
 console.log("Sum example:", sum(5, 7));
+console.log("This is a sample application to demonstrate ESLint configuration.");
 
 module.exports = { sum };
